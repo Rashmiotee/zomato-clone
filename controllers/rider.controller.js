37b54d87d07cb590ed94869fb8e9fadaeaf2be3c
Rashmi_otee <rashmiotee55@gmail.com>
2025-01-22 11:05:40 +0530

    update

--- controllers/rider.controller.js
+++ controllers/rider.controller.js
@@ -9,8 +9,16 @@ exports.getOrders = asyncHandler(async (req, res) => {
     const result = await Order
         .find({ rider: req.user })
         .select("-rider -createdAt -updatedAt -__v")//select find pay lagtey je nahi patvycha ahe tey selct mdhe takhych
-        .populate("resturant", "name hero") //populate main second arugument//joins 
+        .populate("resturant", "name hero mobile address") //populate main second arugument//joins 
+        .populate("customer", "name mobile address") //populate main second arugument//joins 
         .populate("items.dish", "name type image price")//je patvycha ahe tey populate mdhe
         .sort({ createdAt: -1 })
     res.json({ message: "order fetch success", result })
+})
+exports.updateOrderStatus = asyncHandler(async (req, res) => {
+
+    const { oid } = req.params
+    await Order.findByIdAndUpdate(oid, { status: req.body.status })
+
+    res.json({ message: "order update status" })
 })
--- routes/rider.routes.js
+++ routes/rider.routes.js
@@ -1,9 +1,10 @@
-const { getOrders } = require("../controllers/rider.controller")
+const { getOrders, updateOrderStatus } = require("../controllers/rider.controller")
 
 const router = require("express").Router()
 
 
 router
     .get("/get-orders", getOrders)
+    .put("/update-order-status/:oid", updateOrderStatus)
 
 module.exports = router
